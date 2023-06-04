e92504a92815038b7aa19064596262d585130a4b
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-06-03 18:29:30 -0700

    add config file + parsing

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,7 @@
 import { Command } from 'commander';
-import path from 'path';
 import {Prismify} from '../src/prismify';
+import path from 'path';
+import fs from 'fs';
 
 const program = new Command();
 program.option('-w, --watch', 'Watch for changes in schema files');
@@ -8,9 +9,20 @@ program.option('-w, --watch', 'Watch for changes in schema files');
 program.parse(process.argv);
 const options = program.opts();
 
-const schemaFolderPath = path.join(__dirname, '../test/models');
-const outputFilePath = path.join(__dirname, '../test/prisma', 'schema.prisma');
-const watchMode = options.watch || false;
+let configPath = path.join(process.cwd(), 'prismify.json');
+if (options.config) {
+  configPath = path.resolve(process.cwd(), options.config);
+}
+
+if (!fs.existsSync(configPath)) {
+  console.error(`Configuration file not found: ${configPath}`);
+  process.exit(1);
+}
+
+const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+const schemaFolderPath = config.schemaFolderPath;
+const outputFilePath = config.outputFilePath;
+const watchMode = options.watch || config.watchMode || false;
 
 const prismify = new Prismify(schemaFolderPath, outputFilePath, watchMode);
 prismify.run();
