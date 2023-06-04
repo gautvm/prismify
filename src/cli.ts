9076f06efc45e1a0149733c87e9f6d2d6a76a924
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-06-03 21:43:05 -0700

    refactor: add option to specify schema folder and output file from the command line

--- src/cli.ts
+++ src/cli.ts
@@ -6,11 +6,15 @@ import fs from 'fs';
 const program = new Command();
 program.option('-w, --watch', 'Watch for changes in schema files');
 program.option('-c, --config <path>', 'Path to the configuration file');
+program.option('-s, --schema <path>', 'Path to the schema folder');
+program.option('-o, --output <path>', 'Path to the output file');
 
 program.parse(process.argv);
 const options = program.opts();
 
-let configPath = options.config ? path.resolve(process.cwd(), options.config) : path.join(process.cwd(), 'prismify.json');
+let configPath = options.config
+  ? path.resolve(process.cwd(), options.config)
+  : path.join(process.cwd(), 'prismify.json');
 
 if (!fs.existsSync(configPath)) {
   console.error(`Configuration file not found: ${configPath}`);
@@ -21,10 +25,10 @@ const configContent = fs.readFileSync(configPath, 'utf-8');
 const settings = JSON.parse(configContent);
 
 const config: PrismifyConfig = {
-    schemaFolderPath: settings.schemaFolderPath,
-    outputFilePath: settings.outputFilePath,
-    watchMode: options.watch || settings.watchMode || false,
-}
+  schemaFolderPath: options.schema || settings.schemaFolderPath,
+  outputFilePath: options.output || settings.outputFilePath,
+  watchMode: options.watch || settings.watchMode || false,
+};
 
-const prismify = new Prismify(config)
+const prismify = new Prismify(config);
 prismify.run();
