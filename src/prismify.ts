4e06ca9495f648ad482efbfdb344bc3b29dcc628
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-06-03 17:36:43 -0700

    add support for file watching

--- src/prismify.ts
+++ src/prismify.ts
@@ -2,13 +2,32 @@ import fs from 'fs';
 import path from 'path';
 
 export function mergeSchemas(schemaFolderPath: string, outputFilePath: string) {
-  const files = fs.readdirSync(schemaFolderPath);
+  let initialMergeCompleted = false;
 
-  const schemaContents = files
-    .filter((file) => file.endsWith('.prisma'))
-    .map((file) => fs.readFileSync(path.join(schemaFolderPath, file), 'utf-8'))
-    .join('\n');
+  const mergeAndWriteSchemas = () => {
+    const files = fs.readdirSync(schemaFolderPath);
 
-  fs.writeFileSync(outputFilePath, schemaContents, 'utf-8');
+    const schemaContents = files
+      .filter((file) => file.endsWith('.prisma'))
+      .map((file) => fs.readFileSync(path.join(schemaFolderPath, file), 'utf-8'))
+      .join('\n');
+
+    fs.writeFileSync(outputFilePath, schemaContents, 'utf-8');
+  };
+
+  const handleSchemaChange = () => {
+    if (initialMergeCompleted) {
+      mergeAndWriteSchemas();
+      console.log(`Unified schema file '${outputFilePath}' regenerated.`);
+    } else {
+      initialMergeCompleted = true;
+    }
+  };
+
+  mergeAndWriteSchemas();
+  initialMergeCompleted = true;
+
+  fs.watch(schemaFolderPath, { recursive: true }, handleSchemaChange);
+
+  console.log(`Watching for changes in '${schemaFolderPath}'.`);
 }
-
