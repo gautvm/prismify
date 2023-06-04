aad48403420e94fe2da4566902b098f8b9732014
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-06-03 21:35:40 -0700

    refactor: using config interface now

--- src/cli.ts
+++ src/cli.ts
@@ -1,28 +1,30 @@
-import { Command } from "commander";
-import { Prismify } from "../src/prismify";
-import path from "path";
-import fs from "fs";
+import { Command } from 'commander';
+import { Prismify, PrismifyConfig } from '../src/prismify';
+import path from 'path';
+import fs from 'fs';
 
 const program = new Command();
-program.option("-w, --watch", "Watch for changes in schema files");
+program.option('-w, --watch', 'Watch for changes in schema files');
+program.option('-c, --config <path>', 'Path to the configuration file');
 
 program.parse(process.argv);
 const options = program.opts();
 
-let configPath = path.join(process.cwd(), "prismify.json");
-if (options.config) {
-  configPath = path.resolve(process.cwd(), options.config);
-}
+let configPath = options.config ? path.resolve(process.cwd(), options.config) : path.join(process.cwd(), 'prismify.json');
 
 if (!fs.existsSync(configPath)) {
   console.error(`Configuration file not found: ${configPath}`);
   process.exit(1);
 }
 
-const config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
-const schemaFolderPath = config.schemaFolderPath;
-const outputFilePath = config.outputFilePath;
-const watchMode = options.watch || config.watchMode || false;
+const configContent = fs.readFileSync(configPath, 'utf-8');
+const settings = JSON.parse(configContent);
 
-const prismify = new Prismify(schemaFolderPath, outputFilePath, watchMode);
+const config: PrismifyConfig = {
+    schemaFolderPath: settings.schemaFolderPath,
+    outputFilePath: settings.outputFilePath,
+    watchMode: options.watch || settings.watchMode || false,
+}
+
+const prismify = new Prismify(config)
 prismify.run();
